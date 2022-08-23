e085e24a2d82dcd951ff0b1ae7e1a98fec776734
Mateo Montanez Baron <mateomontabaron@gmail.com>
2022-08-23 09:58:38 +0200

    Adding rectangles

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 async function draw(el) {
   // Data
-  const dataset = await d3.json('data.json')
+  const dataset = await d3.json('data.json');
 
   // Dimensions
   let dimensions = {
@@ -8,11 +8,27 @@ async function draw(el) {
     height: 150,
   };
 
+  const box = 30;
+
   // Draw Image
   const svg = d3.select(el)
     .append("svg")
     .attr("width", dimensions.width)
-    .attr("height", dimensions.height)
+    .attr("height", dimensions.height);
+
+  // Rectangles
+  svg.append('g')
+    .attr('transform', 'translate(2,2)')
+    .attr('stroke', 'black ')
+    .attr('fill', '#ddd')
+    .selectAll('rect')
+    .data(dataset)
+    .join('rect')
+    .attr('width', box - 3)
+    .attr('height', box - 3)
+    .attr('x', (d, idx) => box * (idx % 20)) //0, 30, 60
+    .attr('y', (d, idx) => box * ((idx / 20) | 0))
+
 }
 
 draw('#heatmap1')
