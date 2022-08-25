c0bd077a1adef02418752874e2751d25a4b21517
Mateo Montanez Baron <mateomontabaron@gmail.com>
2022-08-25 12:48:45 +0200

    Explanation

--- app.js
+++ app.js
@@ -24,34 +24,33 @@ async function draw(el, scale) {
     case 'quantize':
       colorScale = d3.scaleQuantize()
         .domain(d3.extent(dataset))
-        .range(['#FFF2F2', '#EF9F9F', '#F47C7C'])
+        .range(['#d0e4ff', '#7dace9', '#3682e6'])
       break;
     case 'quantile':
       colorScale = d3.scaleQuantile()
         .domain(dataset)
-        .range(['#FFF2F2', '#EF9F9F', '#F47C7C'])
+        .range(['#d0e4ff', '#7dace9', '#3682e6'])
       break;
     case 'threshold':
       colorScale = d3.scaleThreshold()
         .domain([45200, 135600])
-        .range(['#FFF2F2', '#EF9F9F', '#F47C7C'])
+        .range(['#d0e4ff', '#7dace9', '#3682e6'])
       break;
     case 'linear':
     default:
       colorScale = d3.scaleLinear()
         .domain(d3.extent(dataset))
-        .range(['#FFF2F2', '#F47C7C'])
+        .range(['#d0e4ff', '#3682e6'])
   }
 
   // Rectangles
   svg.append('g')
-    .attr('transform', 'translate(2,2)')
-    .attr('stroke', 'black ')
+    .attr('transform', 'translate(1,1)')
     .selectAll('rect')
     .data(dataset)
     .join('rect')
-    .attr('width', box - 3)
-    .attr('height', box - 3)
+    .attr('width', box - 2)
+    .attr('height', box - 2)
     .attr('x', (d, idx) => box * (idx % 20)) //0, 30, 60
     .attr('y', (d, idx) => box * ((idx / 20) | 0))
     .attr('fill', colorScale)
